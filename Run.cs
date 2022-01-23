99bcfd5f6179da50f2d18375061c46e6fb1cd7ae
Adisesha Manchala (Work) <amanchala@apexanalytix.com>
2022-01-23 18:11:53 -0500

    Changes to Improve logic for re-run

--- Run.cs
+++ Run.cs
@@ -11,15 +11,11 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!! Input a number .... ");
             //args = Console.ReadLine();
             try
             {
-                String a = Console.ReadLine(); //Error
-                if (Convert.ToInt32(a) == 5) //Error
-                {
-                    Console.WriteLine("The Number is {0}", a);
-                }
+                //
+                CompareWith5();
             }
             catch (Exception ex)
             {
@@ -31,8 +27,7 @@ namespace ConsoleApp
 
             try
             {
-                String a = Console.ReadLine();
-                ReRun(a[0]);
+                ReRun();
             }
             catch (Exception ex)
             {
@@ -40,10 +35,53 @@ namespace ConsoleApp
             }
         }
 
-        private static void ReRun(char args)
-        {
-            Console.WriteLine("Do you want to re-run? (Y/N)" , new NotImplementedException());
+        private static void ReRun()
+        {            
+            //Console.WriteLine("Do you want to re-run? (Y/N) \n {0}", new NotImplementedException());
+            Console.WriteLine("Do you want to re-run? (Y/N)");
+            string a = Console.ReadLine();
+            if (char.IsLetter(a[0])
+                && ((char.IsUpper(a[0]) && a[0].Equals('Y'))
+                || (char.IsLower(a[0]) && a[0].Equals('y')))
+                )
+            {
+                //Console.WriteLine("char.IsLetter(a[0]) : {0} /n" +
+                //    "(char.IsUpper(a[0]) : {1} /n" +
+                //    "a[0].Equals(\"Y\") : {2} /n" +
+                //    "(char.IsLower(a[0]) : {3} /n" +
+                //    "a[0].Equals(\"y\") : {4}", char.IsLetter(a[0]), char.IsUpper(a[0]), a[0].Equals("Y"), char.IsLower(a[0]), a[0].Equals("y")
+                //    );
+                CompareWith5();
+                ReRun();
+            }
+        }
 
+        private static void CompareWith5()
+        {            
+            Console.WriteLine("Hello World!! Input a number .... ");
+            String a = Console.ReadLine();
+            if (Convert.ToInt32(a) == 5) //Error
+            {
+                Console.WriteLine("The Number entered is {0} and is equal to 5.", a);
+            }
+            else
+            {
+                Console.WriteLine("The Number is {0} and is not equal to 5.", a);
+            }
+        }
+
+        private static void CompareWith5(string a)
+        {
+            Console.WriteLine("Hello World!! Input a number .... ");
+
+            if (Convert.ToInt32(a) == 5) //Error
+            {
+                Console.WriteLine("The Number entered is {0} and is equal to 5.", a);
+            }
+            else
+            {
+                Console.WriteLine("The Number is {0} and is not equal to 5.", a);
+            }
         }
     }
 }
