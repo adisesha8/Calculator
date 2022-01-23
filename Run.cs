2d508c5d3dc8bb032b710dbff8ecab01cfe7e230
Adisesha Manchala <amanchala@apexanalytix.com>
2022-01-23 16:28:04 -0500

    Add project files.

---
+++ Run.cs
@@ -0,0 +1,49 @@
+﻿using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Run
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!! Input a number .... ");
+            //args = Console.ReadLine();
+            try
+            {
+                String a = Console.ReadLine(); //Error
+                if (Convert.ToInt32(a) == 5) //Error
+                {
+                    Console.WriteLine("The Number is {0}", a);
+                }
+            }
+            catch (Exception ex)
+            {
+                //throw (ex);
+                Console.WriteLine(ex);
+            }
+
+            //Console.ReadLine();
+
+            try
+            {
+                String a = Console.ReadLine();
+                ReRun(a[0]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static void ReRun(char args)
+        {
+            Console.WriteLine("Do you want to re-run? (Y/N)" , new NotImplementedException());
+
+        }
+    }
+}
