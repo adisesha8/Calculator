e85757e2771193379b7aec984208e88b66555153
Adisesha Manchala (Work) <amanchala@apexanalytix.com>
2022-01-23 16:33:11 -0500

    Commit to First Changes

--- Run.cs
+++ Run.cs
@@ -1,11 +1,11 @@
-﻿using Calculator;
+﻿using ConsoleApp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculator
+namespace ConsoleApp
 {
     class Run
     {
